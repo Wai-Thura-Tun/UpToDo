8c254f67c45725f3605bf8c11ac5c7b62c977982
Wai-Thura-Tun <maxw52493@gmail.com>
2025-12-05 22:24:44 +0700

    Initial Commit

---
+++ UpToDoTests/UpToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  UpToDoTests.swift
+//  UpToDoTests
+//
+//  Created by Wai Thura Tun on 5/12/2568 BE.
+//
+
+import Testing
+@testable import UpToDo
+
+struct UpToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

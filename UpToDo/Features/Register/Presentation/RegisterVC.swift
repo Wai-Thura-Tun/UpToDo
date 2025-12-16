4ff51c1f7ac7902881fc1ca16860a320362360fe
Wai-Thura-Tun <maxw52493@gmail.com>
2025-12-16 23:01:01 +0700

    implmented register screen with social login (google and apple)

---
+++ UpToDo/Features/Register/Presentation/RegisterVC.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterVC.swift
+//  UpToDo
+//
+//  Created by Wai Thura Tun on 16/12/2568 BE.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UpToDo/Features/Register/Presentation/ViewModels/RegisterVM.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterVM.swift
+//  UpToDo
+//
+//  Created by Wai Thura Tun on 16/12/2568 BE.
+//
+
+import Foundation
